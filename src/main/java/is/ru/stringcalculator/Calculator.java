f1c4ea468d168067f9c62dcabc2027b06dc76d1a
Reynir <reynir15@ru.is>
2017-10-13 16:59:34 +0000

    Added test for 1 number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,4 +6,16 @@ public class Calculator {
 		if(numbers.equals("")){
 			return 0;	
 		}
+		else{
+			/*if(numbers.contains(",")){
+				String theNumbers[] = numbers.split(",");
+				return toInt(theNumbers[0])+toInt(theNumbers[1]);
+			}*/
+			return 1;
+			
+		}
+	}
+		private static int toInt(String number){
+			return Integer.parseInt(number);
+		}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -9,4 +9,21 @@ public class CalculatorTest {
 	public void testEmptySrting() {
 		assertEquals(0, Calculator.add(""));
 	}
+
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}
+	/*
+	@Test
+	public void testOneNumeR() {
+		assertEquals(3, Calculator.add("1,2"));
+	}*/
 }
+
+
+/*for(int i ...){
+	if(numbers[] < 1000)
+	continue
+}
+*/
